af48ff6b3ce33267b3960ae86aa47b704fed515c
LRLayan <rameshlayan4@gmail.com>
2025-01-24 04:09:30 +0530

    saveCustomer and getAllCustomers

---
+++ src/reducer/CustomerReducer.ts
@@ -0,0 +1,57 @@
+import {Customer} from "../models/Customer.ts";
+import axios from "axios";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+
+const initialState : Customer[] = [];
+
+const api = axios.create({
+    baseURL : "http://localhost:3002/customer"
+});
+
+export const saveCustomer = createAsyncThunk(
+    'customer/saveCustomer',
+    async (customer: Customer) => {
+        try {
+            const response = await api.post('/post',customer);
+            return response.data;
+        } catch (e) {
+            return console.log('failed save customer', e);
+        }
+    }
+);
+
+export const getAllCustomer = createAsyncThunk(
+    'customer/getAllCustomer',
+    async () => {
+        try {
+            const response = await api.get('/getAll');
+            return response.data;
+        } catch (e) {
+            console.log("failed to get customers!",e);
+        }
+    }
+)
+
+const customerSlice = createSlice({
+   name:'customer',
+   initialState: initialState,
+    reducers: {},
+   extraReducers: (builder) => {
+       builder
+           .addCase(saveCustomer.fulfilled, (state,action) => {
+               state.push(action.payload);
+           })
+           .addCase(saveCustomer.pending,() => {
+               console.log("pending save customer");
+           })
+           .addCase(saveCustomer.rejected,() => {
+               console.log("rejected save customer");
+           })
+           .addCase(getAllCustomer.fulfilled,(state,action) => {
+               return action.payload || [];
+           })
+   }
+});
+
+export default customerSlice.reducer;
+
